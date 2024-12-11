78e2e6c326b29797b31730234d33bda41c084636
franbz1 <franciscorualesp15@gmail.com>
2024-12-11 10:53:05 -0500

    agregar auth a firebase

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"
 
 
 
@@ -14,7 +15,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app)
 
 
 
-export { db, app };
+export { db, app, auth };
