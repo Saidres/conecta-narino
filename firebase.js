4904e0c9b3818c9e79fc6b8b22e07da1f5c27c2d
franbz1 <franciscorualesp15@gmail.com>
2024-12-11 13:43:24 -0500

    Login funcional con google y email

--- firebase.js
+++ firebase.js
@@ -5,17 +5,17 @@ import { getAuth } from "firebase/auth"
 
 
 const firebaseConfig = {
-  apikey: "AIzaSyDPQ-nQEHAYJ1DXXVPplaQX8NFOYHwmFhQ",
-  authDomain: "-narino-hackaton.ficonectarebaseapp.com",
+  apiKey: "AIzaSyDPQ-nQEHAYJ1DXXVPplaQX8NFOYHwmFhQ",
+  authDomain: "conecta-narino-hackaton.firebaseapp.com",
   projectId: "conecta-narino-hackaton",
   storageBucket: "conecta-narino-hackaton.firebasestorage.app",
   messagingSenderId: "806134365221",
-  appId: "1:806134365221:web:78fa6f8a923787a3afd3cc",
+  appId: "1:806134365221:web:78fa6f8a923787a3afd3cc"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 
 
