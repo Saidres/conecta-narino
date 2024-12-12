71a1e95e9d2c2303e579debea80da886f5c96b23
Fili258-oss <marinorubianobotina@gmail.com>
2024-12-11 22:31:53 -0500

    Integracion de asistente virtual

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,7 @@ export function MainNav({ items }: MainNavProps) {
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         {/* <Icons.logo className="h-6 w-6" /> */}
-        <span className="inline-block font-bold">{siteConfig.name}</span>
+        <span className="inline-block font-bold text-spring-green-700">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
         <nav className="flex gap-6">
@@ -26,8 +26,8 @@ export function MainNav({ items }: MainNavProps) {
                   key={index}
                   href={item.href}
                   className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",
-                    item.disabled && "cursor-not-allowed opacity-80"
+                    "flex items-center text-sm",
+                    item.disabled && "cursor-not-allowed opacity-80 text-spring-green-800"
                   )}
                 >
                   {item.title}
--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Retro",
+  name: "Conecta Nariño",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
